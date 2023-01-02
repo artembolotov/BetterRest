dbfea2f63ff2160c01bf1c8ced2ef3f639d44830
Artem Bolotov <teeemon@gmail.com>
2023-01-02 13:22:40 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by artembolotov on 02.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
